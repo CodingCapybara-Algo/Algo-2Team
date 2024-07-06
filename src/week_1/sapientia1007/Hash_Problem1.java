2b257ef1c19056982b88c18604e944d49e5e28e4
sapientia1007 <91904494+sapientia1007@users.noreply.github.com>
2024-07-06 21:50:02 +0900

    First Test Commit

---
+++ src/week_1/sapientia1007/Hash_Problem1.java
@@ -0,0 +1,14 @@
+package week_1.sapientia1007;
+
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/1845
+폰켓몬 - Level 1
+ */
+
+import java.io.*;
+import java.util.*;
+public class Hash_Problem1 {
+    public static void main(String[] args) throws Exception{
+    }
+
+}
