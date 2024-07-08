4f017966de6a4cd2f3d9b4f55173a9834680a641
sapientia1007 <91904494+sapientia1007@users.noreply.github.com>
2024-07-09 00:37:48 +0900

    hash 문풀

--- src/week_1/sapientia1007/Hash_Problem1.java
+++ src/week_1/sapientia1007/Hash_Problem1.java
@@ -8,7 +8,23 @@ https://school.programmers.co.kr/learn/courses/30/lessons/1845
 import java.io.*;
 import java.util.*;
 public class Hash_Problem1 {
-    public static void main(String[] args) throws Exception{
-    }
+    static class Solution {
+        public int solution(int[] nums) {
 
+            int maxNum = nums.length/2; // 최대로 가져갈 수 있는 폰켓몬 수
+
+            HashSet<Integer> ponketmon = new HashSet<>();
+            for (int n : nums) {
+                ponketmon.add(n); // 중복 제거한 총 폰켓몬 종류
+            }
+            if (maxNum > ponketmon.size()) return  ponketmon.size(); // 최대로 가져갈 수 있는 수가 중복 제거한 폰켓몬 종류보다 크다 -> 그만큼의 폰켓몬이 없다 -> 중복 제거한 폰켓몬 리턴
+            else return maxNum;
+        }
+    }
+    public static void main(String[] args) throws Exception{
+        Solution sl = new Solution();
+        System.out.println(sl.solution(new int[]{3, 1, 2, 3}));// 2
+        System.out.println(sl.solution(new int[]{3, 3, 3, 2, 2, 4}));// 3
+        System.out.println(sl.solution(new int[]{3, 3, 3, 2, 2, 2}));// 2
+    }
 }
---
+++ src/week_1/sapientia1007/Hash_Problem2.java
@@ -0,0 +1,38 @@
+package week_1.sapientia1007;
+
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/42576
+완주하지 못한 선수 - Level 1
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class Hash_Problem2 {
+    static class Solution {
+        public String solution(String[] participant, String[] completion) {
+            String answer = "";
+            HashMap<String, Integer> players = new HashMap<>();
+            // 참가자 수 누적 (동명이인 존재)
+            for (int i = 0; i<participant.length; i++) {
+                if (players.containsKey(participant[i])) players.put(participant[i], players.get(participant[i])+1);
+                else players.put(participant[i], 1);
+            }
+            // 완주자 감소 -> 해당 이름의 참가자가 모두 완주했다면 HashMap에서 제거
+            for (int i=0; i<completion.length; i++){
+                players.put(completion[i], players.get(completion[i])-1);
+                if (players.get(completion[i])==0) players.remove(completion[i]);
+            }
+            // 최종 결과 출력
+            for (String name : players.keySet()) answer += name;
+            return answer;
+        }
+
+        public static void main(String[] args) throws Exception {
+            Solution s = new Solution();
+            System.out.println(s.solution(new String[]{"leo", "kiki", "eden"}, new String[]{"eden", "kiki"})); //"leo"
+            System.out.println(s.solution(new String[]{"marina", "josipa", "nikola", "vinko", "filipa"}, new String[]{"josipa", "filipa", "marina", "nikola"})); //"vinko"
+            System.out.println(s.solution(new String[]{"mislav", "stanko", "mislav", "ana"}, new String[]{"stanko", "ana", "mislav"})); //"mislav"
+        }
+    }
+}
