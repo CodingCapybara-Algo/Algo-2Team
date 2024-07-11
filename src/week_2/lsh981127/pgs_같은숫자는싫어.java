cc261a69548ad708866242ff9d5765ebd537d276
Sean Lee <61014396+lsh981127@users.noreply.github.com>
2024-07-11 10:57:51 +0900

    2024.07.11

--- src/week_2/lsh981127/pgs_같은숫자는싫어.java
+++ src/week_2/lsh981127/pgs_같은숫자는싫어.java
@@ -1,8 +1,27 @@
 package week_2.lsh981127;
-
+import java.util.*;
 public class pgs_같은숫자는싫어 {
-    public int[] solution(String[] genres, int[] plays) {
-        int[] answer = {};
-        return answer;
+    public class Solution {
+        public int[] solution(int []arr) {
+            Stack<Integer> st = new Stack<>();
+            st.push(arr[0]);
+            for(int i = 1; i < arr.length; i++) {
+
+                int temp = st.pop();
+                if(temp == arr[i]) {
+                    st.push(temp);
+                } else {
+                    st.push(temp);
+                    st.push(arr[i]);
+                }
+
+            }
+            int[] result = new int[st.size()];
+            for(int i = st.size(); i > 0; i--) {
+                result[i-1] = st.pop();
+            }
+            return result;
+
+        }
     }
 }
