71bbf596ed8c6b9fd5375ac1106e95e632861a07
Sean Lee <61014396+lsh981127@users.noreply.github.com>
2024-07-14 13:07:14 +0900

    프로그래머스 기능 개발 구현 중 1

--- src/week_2/lsh981127/pgs_기능개발.java
+++ src/week_2/lsh981127/pgs_기능개발.java
@@ -1,9 +1,42 @@
 package week_2.lsh981127;
-
-import java.util.Stack;
-
+import java.util.*;
 public class pgs_기능개발 {
-    public class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] date = new int[progresses.length];
+        for(int i = 0 ; i < progresses.length; i++) {
+            int due = 100 - progresses[i];
+            int temp = due / speeds[i];
+            int day = (due % speeds[i]) > 0 ? temp + 1 : temp;
+            date[i] = day;
+        }
 
+        List<Integer> re = new ArrayList<>();
+        int cur = date[0];
+        int count = 0;
+        for(int j = 0; j < date.length; j++) {
+            // if(j == date.length - 1) {
+            //     if(cur >= j) {
+            //         re.add(count+1);
+            //     } else {
+            //         re.add(count);
+            //         re.add(1);
+            //     }
+            //     break;
+            // }
+            if(cur >= date[j]) {
+                count++;
+                continue;
+            }
+
+            cur = date[j];  // 더 오래 걸리는 작업
+            re.add(count);
+            count = 1;
+        }
+
+        int[] result = new int[re.size()];
+        for(int i = 0; i < re.size(); i++) {
+            result[i] = re.get(i);
+        }
+        return result;
     }
 }
