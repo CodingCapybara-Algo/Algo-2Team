e77c43d22c3c9d08bd711431a4d9d7f63888bbeb
Sean Lee <61014396+lsh981127@users.noreply.github.com>
2024-07-15 09:42:35 +0900

    2024_07_15

--- src/week_2/lsh981127/pgs_기능개발.java
+++ src/week_2/lsh981127/pgs_기능개발.java
@@ -3,34 +3,37 @@ import java.util.*;
 public class pgs_기능개발 {
     public int[] solution(int[] progresses, int[] speeds) {
         int[] date = new int[progresses.length];
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+
         for(int i = 0 ; i < progresses.length; i++) {
             int due = 100 - progresses[i];
             int temp = due / speeds[i];
             int day = (due % speeds[i]) > 0 ? temp + 1 : temp;
+            q.offer(day);
             date[i] = day;
         }
 
         List<Integer> re = new ArrayList<>();
-        int cur = date[0];
-        int count = 0;
-        for(int j = 0; j < date.length; j++) {
-            // if(j == date.length - 1) {
-            //     if(cur >= j) {
-            //         re.add(count+1);
-            //     } else {
-            //         re.add(count);
-            //         re.add(1);
-            //     }
-            //     break;
-            // }
-            if(cur >= date[j]) {
-                count++;
-                continue;
-            }
 
-            cur = date[j];  // 더 오래 걸리는 작업
-            re.add(count);
-            count = 1;
+        int cur = q.poll();
+        q.addFirst(cur);
+        int count = 0;
+
+        while(!q.isEmpty()) {
+            int day = q.poll();
+            if(cur >= day) {
+                count++;
+                if(q.isEmpty()) {   // 마지막 날
+                    re.add(count);
+                }
+            } else {
+                cur = day;
+                re.add(count);
+                count = 1;
+                if(q.isEmpty()) {   // 마지막 날
+                    re.add(count);
+                }
+            }
         }
 
         int[] result = new int[re.size()];
