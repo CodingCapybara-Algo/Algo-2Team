0360d43f96813d9a9e661405a9b735facc71c780
Sean Lee <61014396+lsh981127@users.noreply.github.com>
2024-07-16 17:44:05 +0900

    2024_07_16_주석

--- src/week_2/lsh981127/pgs_주식가격.java
+++ src/week_2/lsh981127/pgs_주식가격.java
@@ -12,16 +12,13 @@ public class pgs_주식가격 {
             // 그래서 바로 break를 써서 반복문을 끝낸다. (int[] default 값으로 이미 0이 들어있다.)
 
             int j = i;              // index 값 확인용
-            while(prices[j] >= prices[i]) {
-                if(j == prices.length - 1)
+            while(prices[j] >= prices[i]) {     // 시작 index가 i부터이기에 탐색 범위를 계속 줄인다.
+                if(j == prices.length - 1)      // 마지막 index 도달시 IndexOutOfRange 예외가 안나도록 미리 break
                     break;
                 j++;
             }
-            answer[i] = j - i;
-
+            answer[i] = j - i;                  // j번째 index - i번째 index 크기만큼 감소하지 않았으므로 배열에 기록
         }
-
-
         return answer;
     }
 }
