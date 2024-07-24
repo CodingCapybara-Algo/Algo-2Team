323557636dc757e0705e7af02f67399e9f3f94c3
skwent77 <skwent77@gmail.com>
2024-07-24 16:05:35 +0900

    week 2 소스 skwent77 주석 추가

--- src/week_2/skwent77/PGS_같은숫자는싫어.java
+++ src/week_2/skwent77/PGS_같은숫자는싫어.java
@@ -6,20 +6,26 @@ public class PGS_같은숫자는싫어 {
     public int[] solution(int []arr) {
         int[] answer = {};
         ArrayList<Integer> result = new ArrayList<>();
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        /*
+          for 루프를 사용하여 배열 arr를 순회합니다.
+            현재 원소 arr[i]와 다음 원소 arr[i+1]를 비교합니다.
+            두 원소가 다르면 result 리스트에 현재 원소를 추가합니다.
+
+         */
         for(int i=0;i< (arr.length-1) ; i++){
             if( arr[i]  != arr[i+1]){
                 result.add(arr[i]);
             }
             // 1 1 3. 3 0. 1(5). 1(6) size=7 , arr.length-2 => 5
             if(i==arr.length-2){
-                // if( arr[i] != arr[i+1]){
-
+// 마지막 원소 arr[arr.length - 1]를 result 리스트에 추가합니다.
                 result.add(arr[i+1]);
-
+//
             }
 
         }
+        //result 리스트의 크기만큼 answer 배열을 초기화합니다.
+        //        for 루프를 사용하여 result 리스트의 각 원소를 answer 배열에 복사합니다.
         answer = new int[result.size()];
         for (int i = 0; i < answer.length; i++) {
             answer[i] = result.get(i);
@@ -29,3 +35,6 @@ public class PGS_같은숫자는싫어 {
         return answer;
     }
 }
+
+
+
--- src/week_2/skwent77/PGS_주식가격.java
+++ src/week_2/skwent77/PGS_주식가격.java
@@ -1,23 +1,38 @@
 package week_2.skwent77;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class PGS_주식가격 {
     public int[] solution(int[] prices){
-        int size = prices.length;
-        int[] answer = new int[size];
-        for(int i=0; i<size; i++){
-            int count=0;
-            for(int j=i+1; j<size; j++){
+//        int size = prices.length;
+//        int[] answer = new int[size];
+//        for(int i=0; i<size; i++){
+//            int count=0;
+//            for(int j=i+1; j<size; j++){
+//
+//                if(prices[i]>prices[j]){
+//                    count++;
+//                    break;
+//                }
+//                count++;
+//            }
+//            answer[i] = count;
+//        }
+//
+//        return answer;
 
-                if(prices[i]>prices[j]){
-                    count++;
-                    break;
-                }
-                count++;
-            }
-            answer[i] = count;
-        }
+        Queue<Integer> queue = new LinkedList<>();
 
-        return answer;
+
+    }
+
+    public static void main(String[] args) throws Exception{
+        int[] prices = {1,2,3,2,3};
+        // 가격이 떨어지지 않은 기간
+        int[] answer = new int[prices.size()];
+        int[] answer
+        solution(prices);
 
     }
 }
